156b6d9150d6073df880d27556471c7667733d3c
David Teller <david.teller@pasqal.com>
2024-12-27 10:47:35 +0100

    WIP

--- qek/data/datatools.py
+++ qek/data/datatools.py
@@ -10,10 +10,11 @@ import torch
 import torch.utils.data as torch_data
 import torch_geometric.data as pyg_data
 import torch_geometric.utils as pyg_utils
-from qek.data_io.conversion_data import PTCFM_EDGES_MAP, PTCFM_NODES_MAP
-from qek.data_io.dataset import ProcessedData
-from qek.utils import graph_to_mol
 from rdkit.Chem import AllChem
+
+from qek.data.conversion_data import PTCFM_EDGES_MAP, PTCFM_NODES_MAP
+from qek.data.dataset import ProcessedData
+from qek.utils import graph_to_mol
 
 
 def add_graph_coord(
