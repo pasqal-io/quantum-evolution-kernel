0efe0f737fc1767ba861c3ce920eb6c8f0c5a14b
David Teller <david.teller@pasqal.com>
2025-01-06 18:58:22 +0100

    WIP: Applied feedback

--- tests/test_datatools.py
+++ tests/test_datatools.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import networkx as nx
 import torch_geometric.datasets as pyg_dataset
+import torch_geometric.utils as pyg_utils
 
 from qek.data.datatools import add_graph_coord
 from qek.utils import is_disk_graph
@@ -17,10 +19,20 @@ def test_add_graph_coord() -> None:
     for graph in original_ptcfm_data:
         augmented_graph = add_graph_coord(graph=graph, blockade_radius=RADIUS)
 
-    # Some of the graphs from the original data can be converted in disk graphs
-    # by the algorithm, but not all.
-    #
-    # As we know that the first graph can be, we'll use it to test against
-    # regressions.
+        # Make sure that the graph has been augmented with "pos".
+        assert hasattr(augmented_graph, "pos")
+
+        # Confirm that the augmented graph is isomorphic to the original graph.
+        nx_graph = pyg_utils.to_networkx(
+            data=graph,
+            node_attrs=["x"],
+            edge_attrs=["edge_attr"],
+            to_undirected=True,
+        )
+        nx_reconstruct = pyg_utils.to_networkx(augmented_graph).to_undirected()
+
+        assert nx.is_isomorphic(nx_graph, nx_reconstruct)
+
+    # The first graph from the dataset is known to be a disk graph.
     augmented_graph = add_graph_coord(graph=original_ptcfm_data[0], blockade_radius=RADIUS)
     assert is_disk_graph(augmented_graph, RADIUS + EPS)
