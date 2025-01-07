2c402dba023f50eab2e639b2901a9a224edf5d8f
David Teller <david.teller@pasqal.com>
2025-01-07 15:15:17 +0100

    [CI] Let's run the notebook during tests (#10)

---
+++ qek/kernel/__init__.py
@@ -0,0 +1,3 @@
+from .kernel import Kernel
+
+__all__ = ["Kernel"]
--- qek/kernel/kernel.py
+++ qek/kernel/kernel.py
@@ -4,11 +4,12 @@ import collections
 from collections.abc import Sequence
 
 import numpy as np
-from qek_os.data_io.dataset import ProcessedData
 from scipy.spatial.distance import jensenshannon
 
+from qek.data.dataset import ProcessedData
 
-class QekKernel:
+
+class Kernel:
     def __init__(self, mu: float):
         self.mu = mu
 
