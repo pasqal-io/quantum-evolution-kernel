8afd4423fa7294a12e6a7133d6643b65386970f0
David Teller <david.teller@pasqal.com>
2025-02-20 13:30:37 +0100

    [Doc] Uploading the documentation is good, but generating it is fine step, too.

--- qek/kernel/__init__.py
+++ qek/kernel/__init__.py
@@ -1,3 +1,7 @@
 from .kernel import QuantumEvolutionKernel
 
 __all__ = ["QuantumEvolutionKernel"]
+
+"""
+The machine-learning components for this library.
+"""
---
+++ qek/shared/__init__.py
@@ -0,0 +1,7 @@
+"""
+Shared utility code.
+
+Submodules:
+    errors: Exceptions used in this library.
+    utils: Utilities that don't really fit anywhere else.
+"""
